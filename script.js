28cf6d6e22d941e8fd792028c9ad6d4d3bf6c3fc
Frederic Lannes <lannes.frederic@orange.fr>
2023-03-30 18:44:12 +0200

    creation d'objet json pour livres

--- script.js
+++ script.js
@@ -1,38 +1,15 @@
 //créer un livre avec des variables (cour 2)
-// let book;
-// let title = "les miserables";
-// let autor = "Victor Hugo"
-// let price = 14.95;
-// let category = "drame";
-// let releaseDate = new Date(1997,05,23);
-// let nbSales = 345;
+let book;
+let title = "les miserables";
+let autor = "Victor Hugo"
+let price = 14.95;
+let category = "drame";
+let releaseDate = new Date(1997,05,23);
+let nbSales = 345; 
 
 //creer un livre avec des objets et des tableaux (cour 3)
 
-let books = [
-    {
-        id: "0"
-        title: "les miserable",
-        autor: "Victor Hugo",
-        price: 14.95,
-        category: "drame",
-        relaseDate: new Date("1997,05,23"),
-        ndSale: 345,
-        resume:"",
-        vote: [5, 4, 4, 5, 6]
-    },
-    {
-        id: "1"
-        title: "les miserable",
-        autor: "Victor Hugo",
-        price: 14.95,
-        category: "drame",
-        relaseDate: new Date("1997,05,23"),
-        ndSale: 345,
-        resume:"",
-        vote: [5, 4, 4, 5, 6]
-    },
-];
+// let books = []; //importé le json
 
 // pour recupérer le vote 3 de l'objet 1 du tableau
 // console.log(books[0].vote[2]);
